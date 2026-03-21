382aa8a82a00069b4130ede2342a318cb468602d
BeeseChurger <136238535+BEESECHURGERISGOD@users.noreply.github.com>
2026-03-21 08:58:41 -0500

    ahhhhhhhhhh!

--- src/main/java/frc/robot/commands/shoot/LockOnHub.java
+++ src/main/java/frc/robot/commands/shoot/LockOnHub.java
@@ -2,13 +2,9 @@ package frc.robot.commands.shoot;
 
 import java.util.Optional;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
 import com.revrobotics.spark.SparkBase.ControlType;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -27,157 +23,122 @@ public class LockOnHub extends Command {
     private CANDriveSubsystem drive;
     private Vision vision;
     private FuelShooter shooter;
+
     private double targetRPM = 0;
     private boolean lockedOn = false;
+
     private double angleTolerance = Math.toRadians(5);
     private double rpmTolerance = 100;
+
     private double dx = 0;
     private double dy = 0;
-    private Pigeon2 gyro = new Pigeon2(Constants.DriveConstants.PIGEON_2_ID);
 
     public LockOnHub(CANDriveSubsystem drive, Vision vision, FuelShooter shooter) {
-       this.drive = drive;
+        this.drive = drive;
         this.vision = vision;
         this.shooter = shooter;
 
-        addRequirements(drive);
-        addRequirements(vision);
-        addRequirements(shooter);
+        addRequirements(drive, vision, shooter);
     }
 
-    // Called when the command is initially scheduled.
     @Override
     public void initialize() {
         vision.setUsingLimelight(true);
-        vision.CalculateRobotPositionInFieldSpace();
-        gyro.reset();
-
-        if(!vision.hasValidData()){
-            vision.setUsingLimelight(false);
-            return;
-        }
-
-        Vector3 robotLocation = vision.RobotPosInFieldSpace();
-        Vector3 robotRot = vision.RobotRotInFieldSpace();
-
-        Pose3d robotPose = new Pose3d(robotLocation.x, robotLocation.y, robotLocation.z, new Rotation3d(robotRot.x, robotRot.y, robotRot.z));
-        Pose3d shooterPose = robotPose.transformBy(VisionConstants.ROBOT_TO_TURRET);
-
-        // Optional<Alliance> alliance =  DriverStation.getAlliance();
-        // Translation3d targetPose = alliance.isPresent() && alliance.get().equals(Alliance.Blue) ? VisionConstants.BLUE_HUB_POSE : VisionConstants.RED_HUB_POSE;
-
-        // dx = targetPose.getX() - shooterPose.getX();
-        // dy = targetPose.getY() - shooterPose.getY();
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
-    
     @Override
     public void execute() {
 
-        if (!vision.hasValidData()) { //SPIN
-            // drive.driveArcade(0, 0.4); // good for auto but not tele - op 
-                                          // doesnt allow for driver control if april tag isnt seen so best case should just be, do nothing
-            return;
-        }
-        Vector3 robotLocation = vision.RobotPosInFieldSpace();
-        Vector3 robotRot = vision.RobotRotInFieldSpace();
+        if (!vision.hasValidData()) return;
 
-        Pose3d robotPose = new Pose3d(robotLocation.x, robotLocation.y, robotLocation.z, new Rotation3d(robotRot.x, robotRot.y, robotRot.z));
-        Pose3d shooterPose = robotPose.transformBy(VisionConstants.ROBOT_TO_TURRET);
+        // =========================
+        // GET ROBOT STATE
+        // =========================
+        Vector3 robotPos = vision.RobotPosInFieldSpace();
+        double robotHeading = vision.RobotYawRadians();
 
-        Optional<Alliance> alliance =  DriverStation.getAlliance();
-        Translation3d targetPose = alliance.isPresent() && alliance.get().equals(Alliance.Blue) ? VisionConstants.BLUE_HUB_POSE : VisionConstants.RED_HUB_POSE;
+        // =========================
+        // GET TARGET (FIELD SPACE)
+        // =========================
+        Optional<Alliance> alliance = DriverStation.getAlliance();
 
-        dx = targetPose.getX() - shooterPose.getX();
-        dy = targetPose.getY() - shooterPose.getY();
+        Translation3d targetPose =
+            alliance.isPresent() && alliance.get() == Alliance.Blue
+                ? VisionConstants.BLUE_HUB_POSE
+                : VisionConstants.RED_HUB_POSE;
 
-        double angleToTag = Math.atan2(dy, dx);
+        dx = targetPose.getX() - robotPos.x;
+        dy = targetPose.getY() - robotPos.y;
 
-        // this should be the angle of the robot to be compared to target angle
+        // =========================
+        // ANGLE CALC
+        // =========================
+        double angleToTarget = Math.atan2(dy, dx);
 
-        // TODO use this angle to look at the tag.
+        double error = angleToTarget - robotHeading;
+        error = Math.atan2(Math.sin(error), Math.cos(error)); // normalize
 
-        double robotHeading = shooterPose.getRotation().getZ();
-        // Rotation2d robotHeading = gyro.getRotation2d();
-        // double error = angleToTag - robotHeading;
-        // error = Math.atan2(Math.sin(error), Math.cos(error));
-        double error = robotHeading - angleToTag;
+        // =========================
+        // TURN CONTROL
+        // =========================
+        double kP = 0.1;
+        double maxRot = 0.5;
 
-
-        double kP = .1; 
-        double maxRot = 10; 
-
-
-//ready to shoot
         if (Math.abs(error) < angleTolerance) {
-            // drive.driveArcade(0, 0);
+
             lockedOn = true;
 
-            // double distance = Math.hypot(dx, dy);
-            double distance = vision.AprilTagPosInRobotSpace().magnitude() + Constants.CalculateShooterRpmConstants.CAMERA_OFFSET;
+            // =========================
+            // DISTANCE + RPM
+            // =========================
+            double distance = Math.hypot(dx, dy);
+
             targetRPM = -shooter.calculateRPMFromLimelight(distance);
-            SmartDashboard.putNumber("target rpm", targetRPM);
-            SmartDashboard.putNumber("distance", distance);
 
+            shooter.shooterMotorPID.setReference(targetRPM, ControlType.kVelocity);
 
-            shooter.shooterMotorPID.setReference(-targetRPM , ControlType.kVelocity);
+            SmartDashboard.putNumber("Distance", distance);
+            SmartDashboard.putNumber("Target RPM", targetRPM);
 
-
-        //  prints distance and target rpm
-                
-        //  SmartDashboard.putNumber("Shooter Distance", shooter.dx);
-            SmartDashboard.putNumber("Shooter RPM (calc)", targetRPM);
-           return;
+            return;
         }
+
         lockedOn = false;
 
-             double correctionRad = error * kP;
+        double correction = MathUtil.clamp(error * kP, -maxRot, maxRot);
+        drive.driveArcade(0, correction);
 
-             correctionRad = MathUtil.clamp(correctionRad, -maxRot, maxRot);
-
-             drive.driveArcade(0, 0);
-
-        // SmartDashboard.putBoolean("Locked in", lockedOn);
-        SmartDashboard.putNumber("error", Math.toDegrees(error));
-        SmartDashboard.putNumber("angle to tag", angleToTag);
-        // SmartDashboard.putNumber("robot Heading", robotHeading);
+        // =========================
+        // DEBUG
+        // =========================
+        SmartDashboard.putBoolean("Locked On", lockedOn);
+        SmartDashboard.putNumber("Error (deg)", Math.toDegrees(error));
+        SmartDashboard.putNumber("Angle To Target", Math.toDegrees(angleToTarget));
+        SmartDashboard.putNumber("Robot Heading", Math.toDegrees(robotHeading));
         SmartDashboard.putNumber("dx", dx);
         SmartDashboard.putNumber("dy", dy);
-        SmartDashboard.putNumber("shooter pose x", shooterPose.getX());
-        SmartDashboard.putNumber("shooter pose y", shooterPose.getY());
-        SmartDashboard.putNumber("target pose x", targetPose.getX());
-        SmartDashboard.putNumber("target pose y", targetPose.getY());
-
     }
-        
 
-        
-
-    // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        
         vision.setUsingLimelight(false);
-        if(interrupted) {
+
+        if (interrupted) {
             shooter.SetShooterState(FuelShooterState.NONE);
         }
-       // drive.driveArcade(0, 0);
     }
 
-    // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (targetRPM == 0) {
-            return false;
-        }
+        if (targetRPM == 0) return false;
+
         SmartDashboard.putBoolean("Charging Motor", true);
-    
+
         if (Math.abs(shooter.getMotor().getEncoder().getVelocity() - targetRPM) < rpmTolerance) {
             SmartDashboard.putBoolean("Charging Motor", false);
             return true;
-        } 
+        }
+
         return false;
     }
-
 }
--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -1,176 +1,178 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.Vision;
 
 import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
 
+import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.Constants;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.CustomTypes.Math.Vector3;
-
-
 
 public class Vision extends SubsystemBase {
 
   private int aprilTagID = -1;
   private double horizontalOffsetFromAprilTag;
 
+  // TAG (robot space)
   private Vector3 avgAprilTagPosInRobotSpace = Vector3.zero;
-  private ArrayList<Vector3> aprilTagPosInRobotSpaceValues = new ArrayList<Vector3>();
+  private ArrayList<Vector3> aprilTagPosInRobotSpaceValues = new ArrayList<>();
 
-  private Vector3 avgAprilTagRotInRobotSpace = Vector3.zero;
-  private ArrayList<Vector3> aprilTagRotInRobotSpaceValues = new ArrayList<Vector3>();
+  // ROBOT (field space)
+  private Vector3 avgRobotPosInFieldSpace = Vector3.zero;
+  private ArrayList<Vector3> robotPosInFieldSpaceValues = new ArrayList<>();
 
-  private Vector3 avgRobotPosInFieldSpace;
-  private ArrayList<Vector3> robotPosInFieldSpaceValues = new ArrayList<Vector3>();
+  // YAW (radians)
+  private double robotYawRadians = 0.0;
 
-  private Vector3 avgRobotRotInFieldSpace;
-  private ArrayList<Vector3> robotRotInFieldSpaceValues = new ArrayList<Vector3>();
+  public BooleanSupplier HasValidData = () -> hasValidData();
 
-  public BooleanSupplier HasValidData = new BooleanSupplier() {
-    public boolean getAsBoolean() { return hasValidData(); };
-  };
-
-  public static Vision Instance;
   public static boolean usingLimelight = false;
-
   int thingsUsingLimelight = 0;
-
-  // public Vision() 
-  // {
-  //   VisionDriveCalculator.SetVisionReference(this);
-  //   VisionShooterCalculator.SetVisionReference(this);
-
-  //   Instance = this;
-  // }
 
   @Override
   public void periodic() {
-    aprilTagID = (int)NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(-1.0);
-    horizontalOffsetFromAprilTag = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    var table = NetworkTableInstance.getDefault().getTable("limelight");
 
-    if (aprilTagID != -1)
-    {
+    aprilTagID = (int) table.getEntry("tid").getDouble(-1.0);
+    horizontalOffsetFromAprilTag = table.getEntry("tx").getDouble(0);
+    double tv = table.getEntry("tv").getDouble(0);
+
+    if (tv == 1) {
       CalculateRobotPositionInFieldSpace();
       CalculateTargetTransformInRobotSpace();
-      CalculateRobotPositionInFieldSpace();
-    }
-    else
-    {
+    } else {
       ClearAprilTagData();
     }
 
     LogData();
   }
 
-  void CalculateTargetTransformInRobotSpace()
-  {
-    double[] vals = NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_robotspace").getDoubleArray(new double[6]);
-    if(vals[0]!=0){
-      addVector3ToArrayList(new Vector3(vals[0], vals[1], vals[2]), aprilTagPosInRobotSpaceValues);
-      addVector3ToArrayList(new Vector3(vals[3], vals[4], vals[5]), aprilTagRotInRobotSpaceValues);
+  // =========================
+  // TAG POSITION (robot space)
+  // =========================
+  void CalculateTargetTransformInRobotSpace() {
+    var table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    double[] vals = table.getEntry("targetpose_robotspace").getDoubleArray(new double[6]);
+
+    if (vals.length == 6) {
+
+      Vector3 pos = new Vector3(vals[0], vals[1], vals[2]);
+
+      addFilteredSample(pos, 0.5);
       avgAprilTagPosInRobotSpace = getAverageOfArrayList(aprilTagPosInRobotSpaceValues);
-      avgAprilTagRotInRobotSpace = getAverageOfArrayList(aprilTagRotInRobotSpaceValues);
-    } else {
-      System.out.println("Bad Data");
     }
   }
 
-  public void CalculateRobotPositionInFieldSpace()
-  {
-    double[] vals = NetworkTableInstance.getDefault().getTable("limelight").getEntry("botpose").getDoubleArray(new double[6]);
-    //botpose - x,y,z, roll, pitch, yaw
-    if (vals[0]!=0) {
-       addVector3ToArrayList(new Vector3(vals[0], vals[1], vals[2]), robotPosInFieldSpaceValues);
-      //addVector3ToArrayList(new Vector3(vals[2], vals[0] * -1, vals[1] * -1), robotPosInFieldSpaceValues);
+  // =========================
+  // ROBOT POSITION (field space)
+  // =========================
+  public void CalculateRobotPositionInFieldSpace() {
 
-      addVector3ToArrayList(new Vector3(vals[3], vals[4], vals[5]), robotRotInFieldSpaceValues);
+    var table = NetworkTableInstance.getDefault().getTable("limelight");
+
+    double[] vals = table.getEntry("botpose").getDoubleArray(new double[6]);
+
+    if (vals.length == 6) {
+
+      Vector3 pos = new Vector3(vals[0], vals[1], vals[2]);
+
+      addFilteredRobotSample(pos, 1.0);
       avgRobotPosInFieldSpace = getAverageOfArrayList(robotPosInFieldSpaceValues);
-      avgRobotRotInFieldSpace = getAverageOfArrayList(robotRotInFieldSpaceValues);
-    } else {
-      System.out.println("Bad Data");
+
+      robotYawRadians = Math.toRadians(vals[5]);
     }
-    
   }
 
-  void addVector3ToArrayList(Vector3 newVal, ArrayList<Vector3> arrayList)
-  {
-    arrayList.add(0, newVal);
-    if (arrayList.size() > VisionConstants.VALUES_TO_AVERAGE) {arrayList.remove(arrayList.size() - 1); }
-  }
-
-  Vector3 getAverageOfArrayList(ArrayList<Vector3> arrayList)
-  {
-    Vector3 out = Vector3.zero;
-
-    for (int i = 0; i < arrayList.size(); i++)
-    {
-      out = out.add(arrayList.get(i));
+  // =========================
+  // FILTERS
+  // =========================
+  void addFilteredSample(Vector3 newPos, double maxDelta) {
+    if (!aprilTagPosInRobotSpaceValues.isEmpty()) {
+      Vector3 avg = getAverageOfArrayList(aprilTagPosInRobotSpaceValues);
+      if (Vector3.distance(newPos, avg) > maxDelta) return;
     }
 
-    if (arrayList.size() == 0) { return Vector3.zero; }
-    return out.divideBy(arrayList.size());
+    aprilTagPosInRobotSpaceValues.add(0, newPos);
+
+    if (aprilTagPosInRobotSpaceValues.size() > 5) {
+      aprilTagPosInRobotSpaceValues.remove(aprilTagPosInRobotSpaceValues.size() - 1);
+    }
   }
 
-  public boolean hasValidData()
-  {
+  void addFilteredRobotSample(Vector3 newPos, double maxDelta) {
+    if (!robotPosInFieldSpaceValues.isEmpty()) {
+      Vector3 avg = getAverageOfArrayList(robotPosInFieldSpaceValues);
+      if (Vector3.distance(newPos, avg) > maxDelta) return;
+    }
+
+    robotPosInFieldSpaceValues.add(0, newPos);
+
+    if (robotPosInFieldSpaceValues.size() > 10) {
+      robotPosInFieldSpaceValues.remove(robotPosInFieldSpaceValues.size() - 1);
+    }
+  }
+
+  // =========================
+  // UTIL
+  // =========================
+  Vector3 getAverageOfArrayList(ArrayList<Vector3> arrayList) {
+    if (arrayList.isEmpty()) return Vector3.zero;
+
+    Vector3 sum = Vector3.zero;
+    for (Vector3 v : arrayList) {
+      sum = sum.add(v);
+    }
+
+    return sum.divideBy(arrayList.size());
+  }
+
+  public boolean hasValidData() {
     return aprilTagID != -1;
   }
 
-  void LogData()
-  {
+  void LogData() {
     SmartDashboard.putString("Detected AprilTagID", aprilTagID == -1 ? "None" : String.valueOf(aprilTagID));
-    SmartDashboard.putString("AprilTag position in Robot Space", aprilTagID == -1 ? "N/A" : avgAprilTagPosInRobotSpace.toString(3));
-    SmartDashboard.putString("AprilTag rotation in Robot Space", aprilTagID == -1 ? "N/A" : avgAprilTagRotInRobotSpace.toString(3));
-    SmartDashboard.putString("AprilTag horizontal offset", aprilTagID == -1 ? "N/A" : String.valueOf(horizontalOffsetFromAprilTag));
+    SmartDashboard.putString("AprilTag position (robot)", aprilTagID == -1 ? "N/A" : avgAprilTagPosInRobotSpace.toString(3));
+    SmartDashboard.putNumber("Robot Yaw (rad)", robotYawRadians);
     SmartDashboard.putNumber("Things using limelight", thingsUsingLimelight);
   }
 
-  void ClearAprilTagData()
-  {
+  public void ClearAprilTagData() {
     aprilTagPosInRobotSpaceValues.clear();
-    aprilTagRotInRobotSpaceValues.clear();
+    robotPosInFieldSpaceValues.clear(); // ✅ important
   }
 
-  public void OnTeleopDisable()
-  {
+  public void OnTeleopDisable() {
     thingsUsingLimelight = 0;
     ToggleLimelightLight(false);
   }
 
-  public void setUsingLimelight(boolean usingLimelight)
-  {
-    thingsUsingLimelight += usingLimelight ? 1 : -1;
+  public void setUsingLimelight(boolean using) {
+    thingsUsingLimelight += using ? 1 : -1;
 
-    System.out.println(usingLimelight ? "New thing using limelight" : "Something stopped using limelight");
-
-    if (thingsUsingLimelight <= 0) 
-    { 
-      thingsUsingLimelight = 0; 
+    if (thingsUsingLimelight <= 0) {
+      thingsUsingLimelight = 0;
       ToggleLimelightLight(false);
+    } else {
+      ToggleLimelightLight(true);
     }
-    else if (thingsUsingLimelight > 0) { ToggleLimelightLight(true); } 
   }
 
-  public void ToggleLimelightLight(boolean on)
-  {
-    System.out.println("Toggling Limelight " + (on ? "on" : "off"));
-
+  public void ToggleLimelightLight(boolean on) {
     usingLimelight = on;
-    NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(on ? 3 : 1);
+    NetworkTableInstance.getDefault().getTable("limelight")
+        .getEntry("ledMode").setNumber(on ? 3 : 1);
   }
 
-  // Getter methods //
+  // =========================
+  // GETTERS
+  // =========================
   public int AprilTagID() { return aprilTagID; }
-  public Vector3 AprilTagPosInRobotSpace() { return avgAprilTagPosInRobotSpace; } //
-  public Vector3 AprilTagRotInRobotSpace() { return avgAprilTagRotInRobotSpace; } //
-  public Vector3 RobotPosInFieldSpace() { return avgRobotPosInFieldSpace; } //
-  public Vector3 RobotRotInFieldSpace() { return avgRobotRotInFieldSpace; } //
-  public double HorizontalOffsetFromAprilTag() { return horizontalOffsetFromAprilTag; } //
+  public Vector3 AprilTagPosInRobotSpace() { return avgAprilTagPosInRobotSpace; }
+  public Vector3 RobotPosInFieldSpace() { return avgRobotPosInFieldSpace; }
+  public double RobotYawRadians() { return robotYawRadians; }
+  public double HorizontalOffsetFromAprilTag() { return horizontalOffsetFromAprilTag; }
 }
